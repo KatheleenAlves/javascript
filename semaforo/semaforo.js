b65af39f1b76c1047dba682a2ade347159c8bfe6
KatheleenAlves <katheleen.alves@hotmail.com>
2024-08-20 14:00:51 -0300

    Iniciada a função

---
+++ semaforo/semaforo.js
@@ -0,0 +1,10 @@
+const img = document.getElementById("img")
+//Capturando o container pai para depois descobrir os filhos(botões)
+const buttons = document.getElementById("buttons")
+
+const trafficLight = () => {
+    
+}
+
+buttons.addEventListener("click", traffic-light)
+
