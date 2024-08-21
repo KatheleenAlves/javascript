4659c549d6ffc8c146529193353d5aa0ab0b7d79
KatheleenAlves <katheleen.alves@hotmail.com>
2024-08-21 13:57:07 -0300

    Criando as funções

--- semaforo/semaforo.js
+++ semaforo/semaforo.js
@@ -2,9 +2,23 @@ const img = document.getElementById("img")
 //Capturando o container pai para depois descobrir os filhos(botões)
 const buttons = document.getElementById("buttons")
 
-const trafficLight = () => {
+const trafficLight = (event) => {
+   turnOn[event.target.id](); //Chamando evento
+}
+
+//Função dentro de objeto
+const turnOn = {
+    'red': () => img.src = 'img/vermelho.png',
+    'yellow': () => img.src = 'img/amarelo.png',
+    'green': () => img.src = 'img/verde.png',
+    'automatic': () => changeColor() 
+}
+
+const changeColor = () =>{
     
 }
 
-buttons.addEventListener("click", traffic-light)
+setInterval(trocaDeCor, 3000)
+
+buttons.addEventListener("click", trafficLight)
 
