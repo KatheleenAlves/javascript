a4f425e8e3f1b97e737bd85e60ef4d3a406a8249
KatheleenAlves <katheleen.alves@hotmail.com>
2024-08-25 16:00:28 -0300

    Botão automático finalizado

--- semaforo/semaforo.js
+++ semaforo/semaforo.js
@@ -1,20 +1,44 @@
 const img = document.getElementById("img")
 //Capturando o container pai para depois descobrir os filhos(botões)
 const buttons = document.getElementById("buttons")
+let colorIndex = 0
+let intervalId = null
 
 const trafficLight = (event) => {
-   turnOn[event.target.id](); //Chamando evento
+    stopAutomatic() //Toda vez que o evento for chamado, o stop automatic zera o intervalo
+    turnOn[event.target.id](); //Chamando evento
+}
+
+const nextIndex = () =>{
+
+   // colorIndex = colorIndex < 2 ? ++colorIndex : 0  // se for menor que 2 incrementa, se não 0
+
+    if (colorIndex < 2){
+        colorIndex++
+    }else{
+        colorIndex = 0
+    }
+}
+
+const changeColor = () =>{
+    const colors = ['red', 'yellow', 'green']
+    const color = colors[colorIndex]
+    turnOn[color]()
+    nextIndex();
+
+}   
+
+const stopAutomatic = () => {
+    clearInterval(intervalId)
 }
 
 //Função dentro de objeto
 const turnOn = {
     'red': () => img.src = 'img/vermelho.png',
     'yellow': () => img.src = 'img/amarelo.png',
-    'green': () => img.src = 'img/verde.png'
-}
-
-const changeColor = () =>{
-
+    'green': () => img.src = 'img/verde.png',
+    // 'automatic': () => setTimeout(changeColor, 1000)  Executaria uma vez daqui a 1000ms
+    'automatic': () => intervalId = setInterval(changeColor, 1000)   //Executa a cada 1000ms, intervalId armazena o intervalo do id, pq cada vez que clicamos gera um id
 }
 
 
