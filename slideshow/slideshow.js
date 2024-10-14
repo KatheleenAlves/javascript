2417b70b41ff6d84d8773cd23e886593b89e613f
KatheleenAlves <katheleen.alves@hotmail.com>
2024-10-14 14:58:21 -0300

    Criando o array com as imagens

---
+++ slideshow/slideshow.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const images = [
+    {'id': '1', 'url':'./img/chrono.jpg'},{'id': '2', 'url':'./img/inuyasha.jpg'},
+    {'id': '3', 'url':'./img/chrono.jpg'},
+    {'id': '4', 'url':'./img/chrono.jpg'},
+]
+
+loadImages(images)
