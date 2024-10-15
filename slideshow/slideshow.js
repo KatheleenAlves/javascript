0a8f905166e4ba336244bbffcfdb5d0a68c00bd3
KatheleenAlves <katheleen.alves@hotmail.com>
2024-10-15 14:49:09 -0300

    Carregando as imagens, através de uma função

--- slideshow/slideshow.js
+++ slideshow/slideshow.js
@@ -1,9 +1,28 @@
 'use strict'
 
 const images = [
-    {'id': '1', 'url':'./img/chrono.jpg'},{'id': '2', 'url':'./img/inuyasha.jpg'},
-    {'id': '3', 'url':'./img/chrono.jpg'},
-    {'id': '4', 'url':'./img/chrono.jpg'},
+    {'id': '1', 'url':'img/chrono.jpg'},
+    {'id': '2', 'url':'img/inuyasha.jpg'},
+    {'id': '3', 'url':'img/tenchi.jpg'},
+    {'id': '4', 'url':'img/tenjhotenge.jpg'},
+    {'id': '5', 'url':'img/yuyuhakusho.jpg'},
+    {'id': '6', 'url':'img/ippo.jpg'},
 ]
 
-loadImages(images)
+
+const container = document.getElementById("container-items")
+
+//Função carregar imagme
+
+const loadImages = (images, container) => {
+    images.forEach(image => {
+        container.innerHTML += `
+            <div class="item">
+                <img src="${image.url}">
+            </div>
+        `
+    })
+}
+
+
+loadImages(images, container)
